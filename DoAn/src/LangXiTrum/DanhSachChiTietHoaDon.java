c8327757ec1b079468c6235aee09fc94ed318545
mhien1556 <3124410088@sv.sgu.edu.vn>
2025-11-05 10:41:58 +0700

    update cthd

--- DoAn/src/LangXiTrum/DanhSachChiTietHoaDon.java
+++ DoAn/src/LangXiTrum/DanhSachChiTietHoaDon.java
@@ -1,78 +1,131 @@
 package LangXiTrum;
-
-import java.io.BufferedReader;
+ import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class DanhSachChiTietHoaDon implements XuLiDuLieu {
-    private ChiTietHoaDon[] dsct;
-    
-    @Override
-    public void xuat() {
-        for (ChiTietHoaDon chiTietHoaDon : dsct) {
-            chiTietHoaDon.hienThi();
-        }
+
+
+public class DanhSachChiTietHoaDon {
+    ChiTietHoaDon[] dsct = new ChiTietHoaDon[0];
+    int n = 0;
+    Scanner sc = new Scanner(System.in);
+
+    public DanhSachChiTietHoaDon() {}
+
+    public void them(ChiTietHoaDon ct) {
+        dsct = Arrays.copyOf(dsct, n + 1);
+        dsct[n] = new ChiTietHoaDon(ct);
+        n++;
     }
-    @Override
-    public void docFile() {
-        try (BufferedReader br = new BufferedReader(
-            new InputStreamReader(new FileInputStream("ChiTietHoaDon.txt"), "UTF-8"))) {
-                dsct = new ChiTietHoaDon[0];
-             String line;
-            int size = 0;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(";");
-                if (parts.length >= 5) {
-                    String maHD = parts[0];
-                    String maSP = parts[1];
-                    String tenSP = parts[2];
-                    int soLuong = Integer.parseInt(parts[3]);
-                    double donGia = Double.parseDouble(parts[4]);
-                    dsct = Arrays.copyOf(dsct, size+1);
-                    dsct[size++] = new ChiTietHoaDon(maHD, maSP, tenSP, soLuong, donGia);
-                }
+
+    public void them(String maHD, String maSP,  String tenSP ,int SoLuong, int DonGia) {
+        dsct = Arrays.copyOf(dsct, n + 1);
+        dsct[n] = new ChiTietHoaDon(maHD, maSP,tenSP, SoLuong, DonGia);
+        n++;
+    }
+
+    public void deleteCT(String maHD, String maSP) {
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (dsct[i].getMaHD().equalsIgnoreCase(maHD)
+                    && dsct[i].getMaSP().equalsIgnoreCase(maSP)) {
+                index = i;
+                break;
             }
-            System.out.println("Doc du lieu chi tiet hoa don thanh cong");
-        } catch (IOException e) {
-            System.out.println("loi doc file: " + e.getMessage());
-        } catch (NullPointerException e) {
-            System.out.println("khong tim thay thu muc chi tiet hoa don");
+        }
+        
+        if (index == -1) {
+            System.out.println("Khong ton tai chi tiet hoa don");
+            return;
+        }
+
+        for (int i = index; i < n - 1; i++) {
+            dsct[i] = dsct[i + 1];
+        }
+
+        dsct = Arrays.copyOf(dsct, n - 1);
+        n--;
+    }
+
+    public void deleteAllHD(String maHD)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(dsct[i].getMaHD().equalsIgnoreCase(maHD))
+            {
+                deleteCT(maHD, dsct[i].getMaSP());
+                i--;
+            }
         }
     }
 
-    @Override
-    public void ghiFile() {
-        try (BufferedWriter bw = new BufferedWriter(
-        new OutputStreamWriter(new FileOutputStream("ChiTietHoaDon.txt", false), "UTF-8"))) {
-            for (ChiTietHoaDon chiTietHoaDon : dsct) {
-                String line = chiTietHoaDon.getMaHD() + ";" +
-                          chiTietHoaDon.getMaSP() + ";" +
-                          chiTietHoaDon.getTenSP() + ";" +
-                          chiTietHoaDon.getSoLuong() + ";" +
-                          chiTietHoaDon.getDonGia() ;
-            bw.write(line);
-            bw.newLine();
+    public ChiTietHoaDon[] findDSCT(String maHD) {
+        ChiTietHoaDon[] result = new ChiTietHoaDon[0];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dsct[i].getMaHD().equalsIgnoreCase(maHD)) {
+                result = Arrays.copyOf(result, index + 1);
+                result[index] = dsct[i];
+                index++;
+            }
         }
-        System.out.println("Da ghi moi toan bo du lieu vao file ChiTietHoaDon.txt!");
-    } catch (IOException e) {
-        System.out.println("Loi ghi file: " + e.getMessage());
+
+        if (index == 0) {
+            return new ChiTietHoaDon[0];
+        }
+
+        return result;
     }
+
+
+    public boolean editSoLuongInChiTiet(String mahd, String masp, int sl)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(dsct[i].getMaHD().equalsIgnoreCase(mahd) && dsct[i].getMaSP().equalsIgnoreCase(masp))
+            {
+                dsct[i].setSoLuong(sl);
+                return true;
+            }
+        }
+
+        return false;
     }
-    @Override
-    public void nhap(int n) {
+
+    public int getTongTienHD(String maHD) {
+        int Tong = 0;
+        for (int i = 0; i < n; i++) {
+            if (dsct[i].getMaHD().equalsIgnoreCase(maHD)) {
+                Tong += dsct[i].getThanhTien();
+            }
+        }
+        return Tong;
     }
-    @Override
-    public void them() {
+
+    public void Xuat() {
+        System.out.printf("%-10s %-10S %-10s %-10s %-10s %-10s\n",
+                "mahd", "masp","tenSP", "soluong", "dongia", "thanhtien");
+        for (int i = 0; i < n; i++) {
+            dsct[i].xuat();
+        }
     }
-    @Override
-    public void sua(String key) {
-    }
-    @Override
-    public void xoa(String key) {
+
+    public void XuatHD(String maHD)
+    {
+        System.out.printf("%-10s %-10s %-10S %-10s %-10s %-10s\n",
+                "mahd", "masp","tenSP", "soluong", "dongia", "thanhtien");
+        for(int i = 0; i < n; i++)
+        {
+            if(dsct[i].getMaHD().equalsIgnoreCase(maHD))
+            {
+                dsct[i].xuat();
+            }
+        }
     }
 }
